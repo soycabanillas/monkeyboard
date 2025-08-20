ccf53896fd49af301c6286851203bac327618c87
Alejandro Cabanillas Barbacil <soycabanillas@gmail.com>
2025-08-20 11:08:12 +0000

    refactor: Add missing parameter type to layout_manager_initialize_nested_layers function

--- src/monkeyboard_layer_manager.h
+++ src/monkeyboard_layer_manager.h
@@ -16,7 +16,7 @@ typedef struct {
     uint8_t layer_total;
 } pipeline_tap_dance_nested_layers_t;
 
-void layout_manager_initialize_nested_layers();
+void layout_manager_initialize_nested_layers(void);
 void layout_manager_add_layer(platform_keypos_t keypos, uint8_t press_id, uint8_t layer);
 void layout_manager_remove_layer_by_keypos(platform_keypos_t keypos);
 void layout_manager_set_absolute_layer(uint8_t layer);
